e6ec9e8d41075881835e5fc4e1282f9652813a3b
Pakulin Sergei <pakylin3@mail.ru>
2026-02-04 16:58:45 +0300

    + task model

---
+++ app/models/task.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlalchemy import DateTime, Text, func
+from sqlalchemy import Enum as SqlEnum
+from sqlalchemy.dialects.postgresql import UUID as PgUUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ..db.base import BaseModel
+from ..enums.task import TaskStatus
+
+
+class Task(BaseModel):
+    __tablename__ = "tasks"
+
+    id: Mapped[UUID] = mapped_column(PgUUID, primary_key=True, default=uuid4)
+    payload: Mapped[str] = mapped_column(Text, nullable=False)
+    status: Mapped[TaskStatus] = mapped_column(
+        SqlEnum(TaskStatus, name="task_status"),
+        default=TaskStatus.PENDING,
+        server_default=TaskStatus.PENDING.value,
+        nullable=False,
+    )
+    result: Mapped[str | None] = mapped_column(Text, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
