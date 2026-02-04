74e3e4cdb94c1160445099d60dab5c1b7114de88
Pakulin Sergei <pakylin3@mail.ru>
2026-02-04 19:19:43 +0300

    import engine for session

--- app/db/session.py
+++ app/db/session.py
@@ -2,6 +2,8 @@ from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
+from .base import engine
+
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 
